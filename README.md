b72ea594525f35cdcd088308bb1d1df7f1344741
I'jaaz Tello <mitello@wccnet.edu>
2019-02-11 11:07:52 -0500

    Initial api version to complement the react application

--- README.md
+++
@@ -1,2 +0,0 @@
-# Post Application
-A simple experimental application using basic Servlet and JDBC technology combined with GSON, Kotlin, and Ajax to create a public board.
---
+++ src/main/java/servlets/MyFilter.kt
@@ -0,0 +1,22 @@
+package servlets
+
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletRequest
+import java.io.IOException
+import javax.servlet.*
+
+
+class MyFilter: Filter {
+
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
+        val request = req as HttpServletRequest
+        val response = res as HttpServletResponse
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"))
+        response.setHeader("Access-Control-Allow-Credentials", "true")
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+        response.setHeader("Access-Control-Max-Age", "3600")
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me")
+        chain.doFilter(req, res)
+    }
+}
